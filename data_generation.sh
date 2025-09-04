cd40b9dbb0954009ce1d57bbc2cd0c5a4fc4cdcd
Jennifer Wang <157986367+Jenniferwli@users.noreply.github.com>
2025-09-04 20:37:36 +0800

    Delete data_generation.sh

--- data_generation.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# 顺次执行Python脚本
-echo "Data preprocessing"
-python ./datavision/data_division.py && \
-python ./datavision/data_division_specifictest.py && \
-
-echo "Data generate successfully."
